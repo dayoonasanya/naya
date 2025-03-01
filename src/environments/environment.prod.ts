9420b02c9e5cffadfe3044e6008d035e72913b82
Adedayo Onasanya <aonasanya@mail.dccc.edu>
2025-03-01 15:45:47 -0600

    Removed exposed API key from history

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    googleMapsApiKey: 'AIzaSyCeDJHU2sf4bH0myRBDmKxr48YbbaKoMAQ'
-  };
-  
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    googleMapsApiKey: 'AIzaSyCeDJHU2sf4bH0myRBDmKxr48YbbaKoMAQ'
-  };
-  
